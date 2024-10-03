ceda7a40be89729073d090d47c8f0d0d102abf07
Emil Leonhard Lauritzen <s231331@dtu.dk>
2024-10-03 10:28:20 +0200

    Initial setup for React Native App

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -54,6 +54,12 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="three"
+        options={{
+          title: ' Tab three', tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
